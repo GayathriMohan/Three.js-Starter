d7bfb9e49da2b5c322968e8c945dd24bb986f5d4
Gayathri Mohan <gayathrimohan@qburst.com>
2017-02-07 13:56:23 +0530

    added dimensions

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ if (window.WebGLRenderingContext) {
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 4;
+var geometry = new THREE.TorusKnotBufferGeometry(10, 3, 64, 8, 2, 3);
+var material = new THREE.MeshLambertMaterial({ color: 0x53868B });
+var torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+camera.position.z = 80;
 
 const pointLight = new THREE.PointLight(0xFFFFFF);
 
@@ -23,11 +23,11 @@ pointLight.position.y = 50;
 pointLight.position.z = 130;
 scene.add(pointLight);
 
+//render function
 var render = function() {
     requestAnimationFrame(render);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
     renderer.render(scene, camera);
 };
-
 render();
